a71137b1cb6af46b50c2be4aafe85d8c96d047b2
Aurelien Blanchard <accdi1.pro@gmail.com>
2026-08-11 21:56:34 -0400

    created board class

---
+++ Board.java
@@ -0,0 +1,25 @@
+public class Board{
+    //board for each white pieces 
+    public long whitePawns;
+    public long whiteBishops;
+    public long whiteKnights;
+    public long whiteRoocks;
+    public long whiteQween;
+    public long whiteKing;
+
+    //board for each black pieces 
+    public long blackPawns;
+    public long blackBishops;
+    public long blackKnights;
+    public long blackRoocks;
+    public long blackQween;
+    public long blackKing;
+
+    //boards for each combined pieces by color
+    public long whiteBoard;
+    public long blackBoard;
+    
+    //combined bords of all pieces
+    public long board;
+
+}
