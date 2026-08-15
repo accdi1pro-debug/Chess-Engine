5d1eb4a604fbe0517541a7bcebedec3d14c3e79a
Aurelien Blanchard <accdi1.pro@gmail.com>
2026-08-15 00:22:45 -0400

    added board representation of the bit boards with the neo design

--- Board.java
+++ Board.java
@@ -1,25 +1,25 @@
 public class Board{
     //board for each white pieces 
-    public long whitePawns;
-    public long whiteBishops;
-    public long whiteKnights;
-    public long whiteRoocks;
-    public long whiteQween;
-    public long whiteKing;
+    public static long whitePawns = 0x0000_0000_0000_ff00L ;
+    public static long whiteBishops = 0x0000_0000_0000_0024L ;
+    public static long whiteKnights = 0x0000_0000_0000_0042L ;
+    public static long whiteRoocks = 0x0000_0000_0000_0081L ;
+    public static long whiteQueen = 0x0000_0000_0000_0008L ;
+    public static long whiteKing= 0x0000_0000_0000_0010L ;
 
     //board for each black pieces 
-    public long blackPawns;
-    public long blackBishops;
-    public long blackKnights;
-    public long blackRoocks;
-    public long blackQween;
-    public long blackKing;
+    public static long blackPawns = 0x00ff_0000_0000_0000L ;
+    public static long blackBishops = 0x4200_0000_0000_0000L ;
+    public static long blackKnights = 0x2400_0000_0000_0000L ;
+    public static long blackRoocks = 0x8100_0000_0000_0000L ;
+    public static long blackQueen = 0x0800_0000_0000_0000L ;
+    public static long blackKing = 0x1000_0000_0000_0000L ;
 
     //boards for each combined pieces by color
-    public long whiteBoard;
-    public long blackBoard;
+    public static long whiteBoard;
+    public static long blackBoard;
     
     //combined bords of all pieces
-    public long board;
+    public static long board;
 
 }
--- Main.java
+++ Main.java
@@ -5,6 +5,21 @@ import javax.swing.*;
 
 public class Main extends JPanel implements ActionListener  { 
     public Timer timer ;
+    public Image whitePawn = new ImageIcon("src/Images/wp.png").getImage();
+    public Image whiteBishop = new ImageIcon("src/Images/wb.png").getImage();
+    public Image whiteKnight = new ImageIcon("src/Images/wkn.png").getImage();
+    public Image whiteRook = new ImageIcon("src/Images/wr.png").getImage();
+    public Image whiteQueen = new ImageIcon("src/Images/wq.png").getImage();
+    public Image whiteKing = new ImageIcon("src/Images/wk.png").getImage();
+
+
+    public Image blackPawn = new ImageIcon("src/Images/bp.png").getImage();
+    public Image blackBishop = new ImageIcon("src/Images/bb.png").getImage();
+    public Image blackKnight = new ImageIcon("src/Images/bkh.png").getImage();
+    public Image blackRook = new ImageIcon("src/Images/br.png").getImage();
+    public Image blackQueen = new ImageIcon("src/Images/bq.png").getImage();
+    public Image blackKing = new ImageIcon("src/Images/bK.png").getImage();
+
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Mine Sweeper");
@@ -12,17 +27,20 @@ public class Main extends JPanel implements ActionListener  {
         
         // Create an instance of your custom class
         Main gamePanel = new Main();
-
+        
         frame.add(gamePanel);
         frame.pack(); // Shrinks the window perfectly around your 600x600 panel
         frame.setLocationRelativeTo(null); // Centers the window on your screen
         frame.setVisible(true);
+        gamePanel.setBackground(Color.BLUE);
+        
     }
 
     public Main (){
         this.setPreferredSize(new Dimension(1000, 1000));
 
         timer = new Timer(10, this); 
+        //test();
         timer.start();
     }
 
@@ -39,20 +57,92 @@ public class Main extends JPanel implements ActionListener  {
         for (int c = 0; c < 8; c++) {
             for (int r = 0; r < 8; r++) {
                 if (r %2 ==0){
-                    g.setColor(Color.black);
+                    g.setColor(new Color(119, 149, 86));
                     if (c %2 ==0){
-                        g.setColor(Color.white);
+                        g.setColor(new Color(235, 236, 208));
                     }
                 }
                 if (r %2 !=0){
-                    g.setColor(Color.white);
+                    g.setColor(new Color(235, 236, 208));
                     if (c %2 ==0){
-                        g.setColor(Color.black);
+                        g.setColor(new Color(119, 149, 86));
                     }
                 }
                 
                 g.fillRect(r*100 + 100 , c*100 +100, 100, 100);
             }
+        }
+        int count = 0;
+
+        int x = 800;
+        int y = 800;
+
+        for (int i = 0; i < 64; i++) {
+
+            drawWhitePieces(x, y, i , g);
+            
+            drawBlackPieces(x, y, i , g);
+
+            
+            x = x - 100;
+            if (count == 7){
+                y = y - 100;
+                x = 800;
+                count = 0;
+            }else{count++;}
+            
+        }
+        
+        
+    }
+    public void test(){
+        for (int i=0; i <64; i++){
+
+            System.out.println(((Board.whitePawns >>> i) & 1L) != 0);
+            
+        }
+    }
+    public void drawWhitePieces(int x,int y,  int i , Graphics g ){
+
+        if (((Board.whitePawns >>> i) & 1L) != 0) {
+            g.drawImage(whitePawn, x, y, 100, 100, this);
+        }
+        if (((Board.whiteBishops >>> i) & 1L) != 0) {
+            g.drawImage(whiteBishop, x, y, 100, 100, this);
+        }
+        if (((Board.whiteRoocks >>> i) & 1L) != 0) {
+            g.drawImage(whiteRook, x, y, 100, 100, this);
+        }
+        if (((Board.whiteKnights >>> i) & 1L) != 0) {
+            g.drawImage(whiteKnight, x, y, 100, 100, this);
+        }
+        if (((Board.whiteQueen >>> i) & 1L) != 0) {
+            g.drawImage(whiteQueen, x, y, 100, 100, this);
+        }
+        if (((Board.whiteKing >>> i) & 1L) != 0) {
+            g.drawImage(whiteKing, x, y, 100, 100, this);
+        }
+    }
+
+    public void drawBlackPieces(int x,int y,  int i , Graphics g ){
+
+        if (((Board.blackPawns >>> i) & 1L) != 0) {
+            g.drawImage(blackPawn, x, y, 100, 100, this);
+        }
+        if (((Board.blackBishops >>> i) & 1L) != 0) {
+            g.drawImage(blackBishop, x, y, 100, 100, this);
+        }
+        if (((Board.blackRoocks >>> i) & 1L) != 0) {
+            g.drawImage(blackRook, x, y, 100, 100, this);
+        }
+        if (((Board.blackKnights >>> i) & 1L) != 0) {
+            g.drawImage(blackKnight, x, y, 100, 100, this);
+        }
+        if (((Board.blackQueen >>> i) & 1L) != 0) {
+            g.drawImage(blackQueen, x, y, 100, 100, this);
+        }
+        if (((Board.blackKing >>> i) & 1L) != 0) {
+            g.drawImage(blackKing, x, y, 100, 100, this);
         }
     }
 }
